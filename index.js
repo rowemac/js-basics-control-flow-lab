9f605407a8764c7817f662a28526cbe19a19d172
Mac Rowe <m.rowe42@gmail.com>
2021-05-25 12:46:27 -0400

    commit tree c1fe6b708e8f137486f9ede9b1251417aa2058b2

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  if (distance <= 400) {
+    return "This one is on me!"
+  }
+  else if (distance > 2000) {
+    if (distance > 2500) {
+      return "No can do."
+    }
+    else {
+      return "I will gladly take your thirty bucks."
+    };
+  };
+};
+
+function ternaryCheckCity(city){
+  // Write your code here!
+  let answer = city == 'NYC' ? 'Ok, sounds good.' : 'No go.';
+  return answer;
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
-}
+  let answer;
+  switch(tip) {
+    case 'generous':
+      answer = "Thank you so much."
+    break;
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+    case 'not as generous':
+      answer = "Thank you."
+    break;
+
+    default:
+      answer = "Bye."
+  };
+  return answer;
+};
